8ca7c2268e99da54311fa39a79a19a83a55836fb
kaziama <professionalgith@gmail.com>
2025-05-18 22:35:32 +0200

    working with DOM

--- realtime_database.js
+++ realtime_database.js
@@ -16,9 +16,16 @@ let buttonEl = document.getElementById('button-element')
 buttonEl.addEventListener('click', function () {
   let inputValue = inputFieldEl.value;
   push(itemInDataBase, inputValue)
+  
+  //showing items on the page
 
-  console.log(`${inputValue} added to database`)
+  let newParagraph = document.createElement('p')
+  let container = document.getElementById('container')
+  container.appendChild(newParagraph).textContent = inputValue
+  //console.log(`${inputValue} added to database`)
+  
 })
+
 
 
 
