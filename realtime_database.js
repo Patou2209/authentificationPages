8553a90bf6ab17af8de3628ea02af7ca3519795e
kaziama <professionalgith@gmail.com>
2025-05-18 22:05:36 +0200

    pusshing exercice

---
+++ realtime_database.js
@@ -0,0 +1,24 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase,ref,push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSetings  = {
+  databaseURL: "https://realtime-database-1f94c-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSetings)
+const database = getDatabase(app)
+const itemInDataBase = ref (database, "items")
+
+
+let inputFieldEl = document.getElementById('input-field')
+let buttonEl = document.getElementById('button-element')
+
+buttonEl.addEventListener('click', function () {
+  let inputValue = inputFieldEl.value;
+  push(itemInDataBase, inputValue)
+
+  console.log(`${inputValue} added to database`)
+})
+
+
+
